c75fc42b029bb34c32a548201de79b1dc95053b4
DMLFrancis <dml.francis@gmail.com>
2024-03-16 22:48:13 +0000

    edit to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
+
 sudo apt update && sudo apt install nodejs npm
+
 # Install pm2 which is a production process manager for Node.js with a built-in load balancer.
 sudo npm install -g pm2
-# stop any instance of our application running currently
-pm2 stop example_app
-# change directory into folder where application is downloaded
+
+# Stop any instance of our application running currently
+pm2 stop www
+
+# Change directory into folder where application is downloaded
 cd DevOpsSec-CA/
+
 # Install application dependencies
 npm install
+
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
-# Start the application with the process name example_app using pm2
-pm2 start ./bin/www --name example_app
+
+# Start the application with the process name (default name will be 'www') using pm2
+pm2 start ./bin/www
